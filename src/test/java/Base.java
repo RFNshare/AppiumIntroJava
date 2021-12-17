679676081121dd8da4a3e0044844f59cdc9b6268
abdullahalfaroque <af.qups@gmail.com>
2021-12-17 19:47:11 +0600

    ...

--- src/test/java/Base.java
+++ src/test/java/Base.java
@@ -10,16 +10,14 @@ import java.net.URL;
 public class Base {
     public static AndroidDriver<AndroidElement> capabilities() throws MalformedURLException {
         DesiredCapabilities cap = new DesiredCapabilities();
-        File appDir = new File("apk");
-        File app =  new File(appDir, "ApiDemos-debug.apk");
+//        File appDir = new File("apk");
+        File app =  new File("apk", "ApiDemos-debug.apk");
         cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Samsung S9+");
         cap.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-        cap.setCapability(MobileCapabilityType.UDID, "95fdd773");
+        cap.setCapability(MobileCapabilityType.UDID, "2220c33e10037ece");
 
         AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
         return driver;
     }
-
-
 }
